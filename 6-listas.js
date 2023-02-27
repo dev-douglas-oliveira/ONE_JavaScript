4fa8f4a700d9e2c54b36c93a3f525668871e45b6
Douglas Oliveira <dougmoroli@gmail.com>
2023-02-27 20:48:54 -0300

    trabalhdo com listas

--- 6-listas.js
+++ 6-listas.js
@@ -9,5 +9,10 @@ console.log(`Trabalhando com listas`);
 const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 
 listaDeDestinos.push(`Curitiba`);
+
 console.log("Destinos possíveis: ");
 console.log(listaDeDestinos);
+
+listaDeDestinos.splice(2, 1);
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[1]);
