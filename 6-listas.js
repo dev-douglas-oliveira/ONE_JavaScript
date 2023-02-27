cb8ea5cffca52923a3917ed3bc541c1be770546f
Douglas Oliveira <dougmoroli@gmail.com>
2023-02-27 20:34:52 -0300

    criando listas

---
+++ 6-listas.js
@@ -0,0 +1,13 @@
+console.log(`Trabalhando com listas`);
+//ctrl + k + c para comentar diversas linhas ao mesmo tempo
+//ctrl + k + u retirar todos os comentario de diversas linhas ao mesmo tempo
+
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
+
+listaDeDestinos.push(`Curitiba`);
+console.log("Destinos possíveis: ");
+console.log(listaDeDestinos);
