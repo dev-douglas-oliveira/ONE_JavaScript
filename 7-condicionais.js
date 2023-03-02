16ef56e876bb6e684626588d37b48ec73cfd880c
Douglas Oliveira <dougmoroli@gmail.com>
2023-03-01 21:30:56 -0300

    ifs encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -1,14 +1,18 @@
 console.log("Trabalhando com condicionais");
 
 const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
-const idadeComprador = 18;
+const idadeComprador = 17;
+const menorAcompanhado = false;
 
 console.log("Destinos possíveis: ");
 console.log(listaDeDestinos);
 
 if (idadeComprador >= 18) {
-    listaDeDestinos.splice(1, 1); //removendo iten
+    listaDeDestinos.splice(1, 1);
     console.log(listaDeDestinos);
+} else if (menorAcompanhado == false) {
+    console.log("Menor desacompanhado não pode comprar");
 } else {
-    console.log("Vc não pode comprar");
+    listaDeDestinos.splice(1, 1);
+    console.log(listaDeDestinos);
 }
